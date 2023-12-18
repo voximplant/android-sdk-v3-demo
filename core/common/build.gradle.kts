827976ffe50af83eda758350a25d2d4f71cb70ac
Oleg Yakovlev <oyakovlev@voximplant.com>
2023-12-18 14:28:21 +0300

    Fixed build.gradle.kts

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.core.ktx)
+    implementation(libs.androidx.core.ktx)
     implementation(libs.hilt.android)
     implementation(libs.kotlinx.coroutines.android)
     ksp(libs.hilt.compiler)
