65b4be9bb6690e1b3a3583394232a7948f044fd0
Oleg Yakovlev <oyakovlev@voximplant.com>
2023-12-28 21:24:10 +0300

    Updated implementation of Voximplant Android SDK

--- core/calls/build.gradle.kts
+++ core/calls/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     implementation(project(":core:common"))
     implementation(project(":core:model"))
 
-    implementation(libs.voximplant.sdk.bom)
+    implementation(platform(libs.voximplant.sdk.bom))
     api(libs.voximplant.sdk.calls)
 
     implementation(libs.androidx.core.ktx)
