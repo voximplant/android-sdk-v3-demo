147ee0d255b5e5253d34d3dc21ec2c2adc441d1b
Oleg Yakovlev <oyakovlev@voximplant.com>
2024-01-16 12:54:59 +0300

    App version updated to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 - 2023, Zingaya, Inc. All rights reserved.
+ * Copyright (c) 2011 - 2024, Zingaya, Inc. All rights reserved.
  */
 
 plugins {
@@ -20,7 +20,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
