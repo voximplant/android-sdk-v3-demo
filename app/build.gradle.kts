89a4b05f1c66b6f9f159f9984963899882e45913
Rodion Kirillov <rkirillov@voximplant.com>
2026-03-02 12:41:08 +0300

    Change versionName to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 21
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
