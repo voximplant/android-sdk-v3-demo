5874ed0dc873968d5d4c11098d4b2b304e0d48f4
Rodion Kirillov <rkirillov@voximplant.com>
2026-07-15 16:59:11 +0300

    Update Voximplant Sdk to 3.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
     defaultConfig {
         applicationId = "com.voximplant.demos.sdk"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.4"
