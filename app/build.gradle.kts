2fe7c003c4e2a274f8d05597df36d3c8dded994c
Rodion Kirillov <rkirillov@voximplant.com>
2026-09-11 14:28:17 +0300

    Update versionName to 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 23
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.4"
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
